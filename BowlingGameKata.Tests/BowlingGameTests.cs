b7d9fccac97fc3fe7afaf5546a2baf5d17e016bf
Karl Speer <cidthecoatrack@gmail.com>
2013-01-08 13:45:25 -0500

    adding repository

---
+++ BowlingGameKata.Tests/BowlingGameTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameKata.Tests
+{
+    [TestClass]
+    public class BowlingGameTests
+    {
+        private BowlingGame game;
+        
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            game = new BowlingGame();
+        }
+
+        [TestMethod]
+        public void GutterGame()
+        {
+            RollMany(0, 20);
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+        [TestMethod]
+        public void AllOnes()
+        {
+            RollMany(1, 20);
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+        private void RollMany(int pins, int Quantity)
+        {
+            for (int i = 0; i < Quantity; i++)
+                game.Roll(pins);
+        }
+
+        [TestMethod]
+        public void OneSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollMany(0, 17);
+
+            Assert.AreEqual(16, game.GetScore());
+        }
+
+        [TestMethod]
+        public void OneStrike()
+        {
+            game.Roll(10);
+            game.Roll(4);
+            game.Roll(3);
+            RollMany(0, 16);
+
+            Assert.AreEqual(24, game.GetScore());
+        }
+
+        [TestMethod]
+        public void SpareInNonFirstFrame()
+        {
+            game.Roll(10);
+            game.Roll(4);
+            game.Roll(3);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(1);
+            RollMany(0, 13);
+
+            Assert.AreEqual(36, game.GetScore());
+        }
+
+        [TestMethod]
+        public void PerfectGame()
+        {
+            RollMany(10, 12);
+
+            Assert.AreEqual(300, game.GetScore());
+        }
+    }
+}
---
+++ BowlingGameKata/BowlingGame.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class BowlingGame
+    {
+        private int[][] Frames = new int[10][];
+        private int FrameIndex = 0;
+        private int RollIndex = 0;
+
+        public BowlingGame()
+        {
+            for (int i = 0; i < Frames.Length; i++)
+                Frames[i] = new int[2];
+            Frames[9] = new int[3];
+        }
+
+        public void Roll(int Pins)
+        {
+            Frames[FrameIndex][RollIndex++] = Pins;
+            if ((RollIndex == 2 && FrameIndex != 9) || IsStrike(Frames[FrameIndex]))
+            {
+                RollIndex = 0;
+                FrameIndex++;
+            }
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(Frames[frame]))
+                    score += 10 + StrikeBonus(frame);
+                else if (IsSpare(Frames[frame]))
+                    score += 10 + SpareBonus(frame);
+                else
+                    score += FrameScore(Frames[frame]);
+            }
+
+            return score;
+        }
+
+        private int StrikeBonus(int frame)
+        {
+            int bonus = Frames[frame + 1][0];
+            if (IsStrike(Frames[frame + 1]))
+                bonus += Frames[frame + 2][0];
+            else
+                bonus += Frames[frame + 1][1];
+            return bonus;
+        }
+
+        private int SpareBonus(int frame)
+        {
+            return Frames[frame + 1][0];
+        }
+
+        private int FrameScore(int[] Frame)
+        {
+            int score = 0;
+            foreach (int Roll in Frame)
+                score += Roll;
+            return score;
+        }
+
+        private bool IsStrike(int[] frame)
+        {
+            return frame[0] == 10;
+        }
+
+        private bool IsSpare(int[] frame)
+        {
+            return frame[0] + frame[1] == 10;
+        }
+    }
+}
